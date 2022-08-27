5043fed6702f0e957fb953c2c3881cdbe9b13d09
Rahmatilla Xudoyberdiyev <xudoyberdiyevrahmatilla@gmail.com>
2022-08-27 13:41:27 +0500

    Wikipedia telegram bot

---
+++ tgbot.py
@@ -0,0 +1,32 @@
+from lib2to3.pgen2.token import AWAIT
+import logging
+import wikipedia
+ 
+from aiogram import Bot, Dispatcher, executor, types
+
+API_TOKEN = 'BOT_TOKEN'
+
+# Configure logging
+logging.basicConfig(level=logging.INFO)
+
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    await message.reply("Assalomu aleykum. Bizning wikipedia botimizga xush kelibsiz!")
+
+
+
+@dp.message_handler()
+async def sendWiki(message: types.Message):
+    try:
+        wikipedia.set_lang("uz")
+        await message.answer(wikipedia.summary(message.text))
+    except:
+        await message.reply("Bunday maqola topilmadi")
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
